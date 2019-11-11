d66ffac7bd17db1358e13efb5370daece042befa
semonec <semonec@gmail.com>
2019-11-11 18:15:16 +0900

    added snake_react_redux

---
+++ sites/snake_react_redux/precache-manifest.cbc78f8c1222d45563a7c71668ff265c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "881dcae01a24a288211611c0f7cd13f7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cb113faabfd6460e7965",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "bf8703b88b46ac7a17d6",
+    "url": "/static/js/2.201f0071.chunk.js"
+  },
+  {
+    "revision": "cb113faabfd6460e7965",
+    "url": "/static/js/main.7d1cc7cb.chunk.js"
+  },
+  {
+    "revision": "dbe5b9ac697b3478944d",
+    "url": "/static/js/runtime-main.1a36574b.js"
+  }
+]);
